3ddf9a38e26b32ecd7f6c9ebdc918b7346813bcd
Nikhil Deepak  Vishwakarma <93042519+nikhil-ji-first@users.noreply.github.com>
2025-07-01 10:22:43 +0530

    Create encryption.py

---
+++ cryptexpy/encryption.py
@@ -0,0 +1,12 @@
+def caesar_encrypt(text, shift):
+    encrypted = ""
+    for char in text:
+        if char.isalpha():
+            shift_base = ord('A') if char.isupper() else ord('a')
+            encrypted += chr((ord(char) - shift_base + shift) % 26 + shift_base)
+        else:
+            encrypted += char
+    return encrypted
+
+def caesar_decrypt(text, shift):
+    return caesar_encrypt(text, -shift)
